eff6210725243f06a6b55e2512416e33ea766924
karn09 <karn09@gmail.com>
2015-05-17 20:52:04 -0400

    initial

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/**
+ * initialize and draw map on screen
+ */
+(function() {
+	var map, infoWindow, marker
+	var newYorkCity = new google.maps.LatLng(40.777151307946326, -73.97487448144528);
+    var initialize = function() {
+        // map options object with default settings centering map on NYC
+        var mapOptions = {
+            center: newYorkCity,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        // set search boundaries
+        var bounds = new google.maps.LatLngBounds(
+        	new google.maps.LatLng(40.70213498801132, -74.02151065429689),
+        	new google.maps.LatLng(40.852167627881336, -73.92823830859368)
+        );
+
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        var autocomplete = new google.maps.places.Autocomplete(input);
+
+
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        map.fitBounds(bounds);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        //autocomplete.bindTo('bounds', map);
+
+        // infoWindow = new google.maps.InfoWindow();
+        // marker = new google.maps.Marker({
+        // 	map: map,
+        // 	anchorPoint: new google.maps.Point(0, -29)
+        // });
+		console.log(searchBox)
+		console.log(autocomplete.getPlace)
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize)
+})()
