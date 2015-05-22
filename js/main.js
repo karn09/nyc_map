1f139cb176efffdab805c22b8b492252105620cc
karn09 <karn09@gmail.com>
2015-05-22 07:56:13 -0400

    messing with autocomplete in maps api

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * initialize and draw map on screen
  */
 (function() {
-	var map, infoWindow, marker
+	var map, infoWindow, marker, autocomplete;
 	var newYorkCity = new google.maps.LatLng(40.777151307946326, -73.97487448144528);
     var initialize = function() {
         // map options object with default settings centering map on NYC
@@ -18,13 +18,21 @@
         );
 
         var input = document.getElementById('pac-input');
+        // create search input on maps window
         var searchBox = new google.maps.places.SearchBox(input);
-        var autocomplete = new google.maps.places.Autocomplete(input);
+        // bind autocomplete to bounds specified
+        autocomplete = new google.maps.places.Autocomplete(input); // 
 
 
         map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
         map.fitBounds(bounds);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        
+        // add listener on address input
+        google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        	getAddress();
+        });
+
         //autocomplete.bindTo('bounds', map);
 
         // infoWindow = new google.maps.InfoWindow();
@@ -32,10 +40,12 @@
         // 	map: map,
         // 	anchorPoint: new google.maps.Point(0, -29)
         // });
-		console.log(searchBox)
-		console.log(autocomplete.getPlace)
-
     }
+
+var getAddress = function () {
+	var place = autocomplete.getPlace();
+	console.log(place)
+}
 
     google.maps.event.addDomListener(window, 'load', initialize)
 })()
