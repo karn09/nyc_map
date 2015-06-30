b99742826d764ac17e196b09e8fabb3fde42a741
John Nieves <karn09@gmail.com>
2015-06-30 22:32:26 +0000

    refactoring code in main2.js

--- js/main.js
+++ js/main.js
@@ -2,36 +2,102 @@
  * initialize and draw map on screen
  */
 (function() {
-	var map, infoWindow, marker, autocomplete;
-	var newYorkCity = new google.maps.LatLng(40.777151307946326, -73.97487448144528);
+    var map, infoWindow, marker, autocomplete, service, searchBox
+    var newYorkCity = new google.maps.LatLng(40.777151307946326, -73.97487448144528);
+    var cache = {};
+
     var initialize = function() {
+        var that = this;
+        that.keyword = ko.observable("");
+        that.results = ko.observableArray();
+
         // map options object with default settings centering map on NYC
+        //var search = ko.observable("");
         var mapOptions = {
             center: newYorkCity,
             zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            types: ['restaurant']
         };
         // set search boundaries
-        var bounds = new google.maps.LatLngBounds(
-        	new google.maps.LatLng(40.70213498801132, -74.02151065429689),
-        	new google.maps.LatLng(40.852167627881336, -73.92823830859368)
+        var NYCbounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(40.70213498801132, -74.02151065429689),
+            new google.maps.LatLng(40.852167627881336, -73.92823830859368)
         );
 
         var input = document.getElementById('pac-input');
         // create search input on maps window
-        var searchBox = new google.maps.places.SearchBox(input);
+        //var searchBox = new google.maps.places.SearchBox(input);
         // bind autocomplete to bounds specified
-        autocomplete = new google.maps.places.Autocomplete(input); // 
-
+        //autocomplete = new google.maps.places.Autocomplete(input);
+        service = new google.maps.places.AutocompleteService({
+            bounds: NYCbounds,
+            types: ['restaurant']
+        });
 
         map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-        map.fitBounds(bounds);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        
-        // add listener on address input
-        google.maps.event.addListener(autocomplete, 'place_changed', function () {
-        	getAddress();
-        });
+        map.fitBounds(NYCbounds);
+        //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        //var serviceObj = new google.maps.places.PlacesService(map);
+
+
+        that.enterSearch = function(d, e) {
+            e.keyCode === 13 && that.search();
+            return true;
+        };
+
+        that.search = function() {
+            service.getPlacePredictions({
+                input: that.keyword()
+            }, function(predictions) {
+                if (that.results().length > 0) {
+                    that.results.removeAll();
+                }
+                predictions.forEach(function(p) {
+                    console.log(p.terms)
+                    var id = p.place_id;
+                    //var pInfo = p.getDetails(id)
+                    that.results.push(
+                        id
+                        // '<div class="panel panel-default">'+
+                        // '<div class="panel-heading"><a href="">'+ p.description + '</a></div></div><hr>'
+                        )
+                })
+            })
+        };
+
+
+
+        // var results = ko.computed(function() {
+        //         if (event.which == 13 || event.keyCode == 13) {
+        //             //code to execute here
+        //             console.log(this.search())
+        //             return false;
+        //         }
+        //         return true;
+        //     }, this)
+        // var results = ko.computed(function() {
+        //     //console.log(this.search())
+        //     //searchBox = new google.maps.places.SearchBox(this.search());
+        //     //autocomplete = new google.maps.places.Autocomplete(this.search());
+        //     //service = new google.maps.places.AutocompleteService();
+        //     service.getPlacePredictions({ 
+        //         input: this.search() 
+        //     }, function(pred) {
+
+        //     }, 'OK')
+        // }, this);
+
+        // function search(input) {
+        //     $('<p>').text('searching for '+input.value).appendTo('body');
+        // }
+
+        // $("#searchForm").submit(function() {
+        //     search($("#searchText").get(0));
+        //     return false;
+        // });
+        //console.log(this.input())
+
 
         //autocomplete.bindTo('bounds', map);
 
@@ -42,10 +108,26 @@
         // });
     }
 
-var getAddress = function () {
-	var place = autocomplete.getPlace();
-	console.log(place)
-}
+    var renderInfo = function(predictions) {
+        var resultList = document.getElementById('results');
+        console.log(predictions)
+        if (resultList.childNodes) {
 
-    google.maps.event.addDomListener(window, 'load', initialize)
+        }
+    };
+
+
+    //http://www.codeproject.com/Articles/351298/KnockoutJS-and-Google-Maps-binding
+    var getAddress = function(input) {
+        //var place = autocompleteService.getQueryPredictions();
+        //console.log(place)
+        service.getQueryPredictions(input, function(pred) {
+            console.log(pred)
+        });
+    };
+
+
+
+    google.maps.event.addDomListener(window, 'load',
+        ko.applyBindings(new initialize))
 })()
---
+++ js/main2.js
@@ -0,0 +1,105 @@
+var ViewModel = {
+    keyword: ko.observable(''),
+    results: ko.observableArray(),
+    points: ko.observableArray(),
+    placeIds: []
+};
+
+ViewModel.enterSearch = function(d, e) {
+    e.keyCode === 13 && this.search();
+    return true
+}
+ViewModel.searchService = function(keyword) {
+    var self = this;
+    var NYCbounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(40.70213498801132, -74.02151065429689),
+        new google.maps.LatLng(40.852167627881336, -73.92823830859368)
+    );
+    var service = new google.maps.places.AutocompleteService({
+        bounds: NYCbounds,
+        types: ['restaurant']
+    });
+
+    service.getPlacePredictions({
+        input: keyword
+    }, function(predictions) {
+        predictions.forEach(function(p) {
+            //console.log(p)
+            // var info = placeService.getDetails(place_id, function(info) {
+            //     console.log(info)
+            // })
+//            self.placeIds.push(p.place_id);
+            //console.log(p.place_id)
+            self.getPlaceDetails(p.place_id)
+        });
+    });
+};
+
+ViewModel.getPlaceDetails = function(id) {
+    var self = this;
+    var mapId = document.getElementById('hold-it'); // have to create an arbitrary element to create PlacesService obj
+    var service = new google.maps.places.PlacesService(mapId);
+    service.getDetails({ placeId: id }, function(info) {
+        
+      self.results.push(info.name)
+      self.addMarker(info.geometry.location)
+    });
+    
+}
+
+ViewModel.addMarker = function(loc) {
+    console.log(loc)    
+    
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0, place; place = places[i]; i++) {
+            var image = {
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            var marker = new google.maps.Marker({
+                icon: image,
+                position: loc
+            });
+
+            markers.push(marker);
+
+            bounds.extend(loc);
+        }
+}
+
+ViewModel.search = function() {
+    this.searchService(this.keyword())
+        //    this.results.push("Results are: " + this.searchService(this.keyword()));
+}
+
+var MapViewModel = {
+    init: function() {
+        var mapId = document.getElementById('map-canvas')
+        var mapOptions = {
+            center: new google.maps.LatLng(40.777151307946326, -73.97487448144528),
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            types: ['restaurant']
+        };
+        var map = new google.maps.Map(mapId, mapOptions);
+        // var service = new google.maps.places.AutocompleteService({
+        //     bounds: NYCbounds,
+        //     types: ['restaurant']
+        // })
+    },
+
+    makeMapMarker: function(m) {
+        var infoWindow = new google.maps.infoWindow();
+    }
+
+}
+
+//window.onload(ko.applyBindings(ViewModel))
+
+window.addEventListener('load', ko.applyBindings(ViewModel))
+google.maps.event.addDomListener(window, 'load', new MapViewModel.init)
