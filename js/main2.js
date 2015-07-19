8e43df4b185ca0e34dd62c7ffd9486a3dc6fa347
karn09 <karn09@gmail.com>
2015-07-19 11:58:40 -0400

    changes to how I link items

--- js/main2.js
+++ js/main2.js
@@ -2,7 +2,8 @@ var ViewModel = {
     keyword: ko.observable(''),
     results: ko.observableArray(),
     points: ko.observableArray(),
-    placeIds: []
+    placeIds: [],
+    locs: ko.observableArray()
 };
 
 ViewModel.enterSearch = function(d, e) {
@@ -40,25 +41,25 @@ ViewModel.getPlaceDetails = function(id) {
     var mapId = document.getElementById('hold-it'); // have to create an arbitrary element to create PlacesService obj
     var service = new google.maps.places.PlacesService(mapId);
     service.getDetails({ placeId: id }, function(info) {
-        
       self.results.push(info.name)
-      self.addMarker(info.geometry.location)
+      console.log(info.geometry.location)
+      self.locs.push(info.geometry.location)
     });
     
 }
 
 ViewModel.addMarker = function(loc) {
-    console.log(loc)    
-    
+             
+        console.log(loc)
         markers = [];
         var bounds = new google.maps.LatLngBounds();
-        for (var i = 0, place; place = places[i]; i++) {
-            var image = {
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
+        // for (var i = 0, place; place = places[i]; i++) {
+        //     var image = {
+        //         size: new google.maps.Size(71, 71),
+        //         origin: new google.maps.Point(0, 0),
+        //         anchor: new google.maps.Point(17, 34),
+        //         scaledSize: new google.maps.Size(25, 25)
+        //     };
 
             // Create a marker for each place.
             var marker = new google.maps.Marker({
@@ -66,27 +67,33 @@ ViewModel.addMarker = function(loc) {
                 position: loc
             });
 
-            markers.push(marker);
+            // markers.push(marker);
 
-            bounds.extend(loc);
+            // bounds.extend(loc);
         }
-}
+//}
 
 ViewModel.search = function() {
-    this.searchService(this.keyword())
+    this.searchService(this.keyword()) // add an event listener here, that onClick 
         //    this.results.push("Results are: " + this.searchService(this.keyword()));
 }
 
 var MapViewModel = {
-    init: function() {
+    init: function(Lat, Lng, z) {
         var mapId = document.getElementById('map-canvas')
         var mapOptions = {
-            center: new google.maps.LatLng(40.777151307946326, -73.97487448144528),
-            zoom: 12,
+            center: new google.maps.LatLng(Lat, Lng),
+            // 40.777151307946326, -73.97487448144528
+            zoom: z,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
             types: ['restaurant']
         };
-        var map = new google.maps.Map(mapId, mapOptions);
+        map: new google.maps.Map(mapId, mapOptions); 
+        // try abstracting into a prototype
+        // maybelook into DI
+        // need to figure out a way to change the center, by overriding the current center in default options.
+        // set map center by using eventlistener attached to each search result.
+
         // var service = new google.maps.places.AutocompleteService({
         //     bounds: NYCbounds,
         //     types: ['restaurant']
@@ -102,4 +109,4 @@ var MapViewModel = {
 //window.onload(ko.applyBindings(ViewModel))
 
 window.addEventListener('load', ko.applyBindings(ViewModel))
-google.maps.event.addDomListener(window, 'load', new MapViewModel.init)
+google.maps.event.addDomListener(window, 'load', new MapViewModel.init(40.777151307946326, -73.97487448144528, 12))
